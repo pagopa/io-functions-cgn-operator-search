12941d6fe4753e09f2a9732d979b409344d803ac
AleDore <alessio85.dore@gmail.com>
2022-01-11 15:03:10 +0100

    refactor

--- GetDiscountBucketCode/__tests__/handler.test.ts
+++ GetDiscountBucketCode/__tests__/handler.test.ts
@@ -1,4 +1,5 @@
 /* tslint:disable: no-any */
+import { Transaction } from "sequelize/types";
 import { DiscountBucketCode } from "../../generated/definitions/DiscountBucketCode";
 import { GetDiscountBucketCodeHandler } from "../handler";
 
@@ -14,19 +15,13 @@ const aDiscountBucketCode = {
   used: false
 };
 
-const selectMockResult = jest.fn().mockImplementation(
-  () =>
-    new Promise(resolve => {
-      resolve([aDiscountBucketCode]);
-    })
-);
+const selectMockResult = jest
+  .fn()
+  .mockImplementation(() => Promise.resolve([aDiscountBucketCode]));
 
-const updateMockResult = jest.fn().mockImplementation(
-  () =>
-    new Promise(resolve => {
-      resolve([undefined, 1]);
-    })
-);
+const updateMockResult = jest
+  .fn()
+  .mockImplementation(() => Promise.resolve([undefined, 1]));
 
 const queryMock = jest.fn().mockImplementation((query: string, params) => {
   if (query.includes("FROM discount_bucket_code")) {
@@ -40,13 +35,17 @@ const queryMock = jest.fn().mockImplementation((query: string, params) => {
   }
 });
 
-const transactionMock = jest.fn().mockImplementation(
-  async <A>(a: () => A): Promise<A> => {
-    return a();
-  }
-);
+const transactionMock = ({
+  commit: jest.fn().mockImplementation(() => Promise.resolve(void 0)),
+  rollback: jest.fn().mockImplementation(() => Promise.resolve(void 0))
+} as unknown) as Transaction;
 
-const cgnOperatorDbMock = { query: queryMock, transaction: transactionMock };
+const cgnOperatorDbMock = {
+  query: queryMock,
+  transaction: jest
+    .fn()
+    .mockImplementation(() => Promise.resolve(transactionMock))
+};
 
 const anExpectedResponse: DiscountBucketCode = { code: aBucketCode };
 
@@ -59,44 +58,41 @@ describe("GetDiscountBucketCodeHandler", () => {
     const response = await GetDiscountBucketCodeHandler(
       cgnOperatorDbMock as any
     )({} as any, aDiscountFk);
-    expect(response.kind).toBe("IResponseSuccessJson");
-    expect(transactionMock).toBeCalledTimes(1);
     expect(queryMock).toBeCalledTimes(2);
     expect(selectMockResult).toBeCalledTimes(1);
     expect(updateMockResult).toBeCalledTimes(1);
+    console.log(response.detail);
+    expect(transactionMock.commit).toBeCalledTimes(1);
+
+    expect(response.kind).toBe("IResponseSuccessJson");
     if (response.kind === "IResponseSuccessJson") {
       expect(response.value).toEqual(anExpectedResponse);
     }
   });
 
   it("should return a IResponseErrorNotFound if no code is found", async () => {
-    selectMockResult.mockImplementationOnce(
-      () =>
-        new Promise(resolve => {
-          resolve([]);
-        })
-    );
+    selectMockResult.mockImplementationOnce(() => Promise.resolve([]));
 
     const response = await GetDiscountBucketCodeHandler(
       cgnOperatorDbMock as any
     )({} as any, aDiscountFk);
     expect(response.kind).toBe("IResponseErrorNotFound");
-    expect(transactionMock).toBeCalledTimes(1);
+    expect(transactionMock.rollback).toBeCalledTimes(1);
     expect(queryMock).toBeCalledTimes(1);
     expect(selectMockResult).toBeCalledTimes(1);
     expect(updateMockResult).not.toHaveBeenCalled();
   });
 
   it("should return a IResponseErrorInternal if any error is thrown during select", async () => {
-    selectMockResult.mockImplementationOnce(() => {
-      throw Error("Any error on select");
-    });
+    selectMockResult.mockImplementationOnce(() =>
+      Promise.reject("Any error on select")
+    );
 
     const response = await GetDiscountBucketCodeHandler(
       cgnOperatorDbMock as any
     )({} as any, aDiscountFk);
     expect(response.kind).toBe("IResponseErrorInternal");
-    expect(transactionMock).toBeCalledTimes(1);
+    expect(transactionMock.rollback).toBeCalledTimes(1);
     expect(queryMock).toBeCalledTimes(1);
     expect(selectMockResult).toBeCalledTimes(1);
     expect(updateMockResult).not.toHaveBeenCalled();
@@ -108,15 +104,15 @@ describe("GetDiscountBucketCodeHandler", () => {
   });
 
   it("should return a IResponseErrorInternal if any error is thrown during update", async () => {
-    updateMockResult.mockImplementationOnce(() => {
-      throw Error("Any error on update");
-    });
+    updateMockResult.mockImplementationOnce(() =>
+      Promise.reject("Any error on update")
+    );
 
     const response = await GetDiscountBucketCodeHandler(
       cgnOperatorDbMock as any
     )({} as any, aDiscountFk);
     expect(response.kind).toBe("IResponseErrorInternal");
-    expect(transactionMock).toBeCalledTimes(1);
+    expect(transactionMock.rollback).toBeCalledTimes(1);
     expect(queryMock).toBeCalledTimes(2);
     expect(selectMockResult).toBeCalledTimes(1);
     expect(updateMockResult).toBeCalledTimes(1);
@@ -128,18 +124,15 @@ describe("GetDiscountBucketCodeHandler", () => {
   });
 
   it("should return a IResponseErrorInternal if no update is executed", async () => {
-    updateMockResult.mockImplementationOnce(
-      () =>
-        new Promise(resolve => {
-          resolve([undefined, 0]);
-        })
+    updateMockResult.mockImplementationOnce(() =>
+      Promise.resolve([undefined, 0])
     );
 
     const response = await GetDiscountBucketCodeHandler(
       cgnOperatorDbMock as any
     )({} as any, aDiscountFk);
     expect(response.kind).toBe("IResponseErrorInternal");
-    expect(transactionMock).toBeCalledTimes(1);
+    expect(transactionMock.rollback).toBeCalledTimes(1);
     expect(queryMock).toBeCalledTimes(2);
     expect(selectMockResult).toBeCalledTimes(1);
     expect(updateMockResult).toBeCalledTimes(1);
--- GetDiscountBucketCode/handler.ts
+++ GetDiscountBucketCode/handler.ts
@@ -3,16 +3,14 @@ import { Context } from "@azure/functions";
 import * as AR from "fp-ts/lib/Array";
 import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
-import * as O from "fp-ts/lib/Option";
-import * as TO from "fp-ts/lib/TaskOption";
-import { flow, pipe } from "fp-ts/lib/function";
+import { pipe } from "fp-ts/lib/function";
 import { ContextMiddleware } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
 import { RequiredParamMiddleware } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/required_param";
 import {
   withRequestMiddlewares,
   wrapRequestHandler
 } from "@pagopa/io-functions-commons/dist/src/utils/request_middleware";
-import { Sequelize, QueryTypes } from "sequelize";
+import { Sequelize, QueryTypes, Transaction } from "sequelize";
 import {
   IResponseErrorInternal,
   IResponseErrorNotFound,
@@ -40,6 +38,16 @@ type IGetDiscountBucketCodeHandler = (
   discountId: string
 ) => Promise<ResponseTypes>;
 
+const rollbackTransaction = (
+  transaction: Transaction
+): TE.TaskEither<Error, void> =>
+  TE.tryCatch(() => transaction.rollback(), E.toError);
+
+const commitTransaction = (
+  transaction: Transaction
+): TE.TaskEither<Error, void> =>
+  TE.tryCatch(() => transaction.commit(), E.toError);
+
 export const GetDiscountBucketCodeHandler = (
   cgnOperatorDb: Sequelize
 ): IGetDiscountBucketCodeHandler => async (
@@ -47,57 +55,74 @@ export const GetDiscountBucketCodeHandler = (
   discountId
 ): Promise<ResponseTypes> =>
   pipe(
-    TE.tryCatch(
-      () =>
-        cgnOperatorDb.transaction(async t =>
-          pipe(
-            await cgnOperatorDb.query(SelectDiscountBucketCodeByDiscount, {
+    TE.tryCatch(() => cgnOperatorDb.transaction(), E.toError),
+    TE.mapLeft(err => ResponseErrorInternal(err.message)),
+    TE.chain(t =>
+      pipe(
+        TE.tryCatch(
+          () =>
+            cgnOperatorDb.query(SelectDiscountBucketCodeByDiscount, {
               model: DiscountBucketCodeModel,
               raw: true,
               replacements: { discount_fk: discountId },
               transaction: t,
               type: QueryTypes.SELECT
             }),
-            AR.head,
-            TO.fromOption,
-            TO.chain(code => async (): Promise<
-              O.Option<DiscountBucketCodeModel>
-            > => {
-              // eslint-disable-next-line @typescript-eslint/no-unused-vars
-              const [__, numberOfUpdatedRecords] = await cgnOperatorDb.query(
-                UpdateDiscountBucketCodeSetUsed,
-                {
+          E.toError
+        ),
+        TE.bimap(err => ResponseErrorInternal(err.message), AR.head),
+        TE.chainW(
+          TE.fromOption(() =>
+            ResponseErrorNotFound("Not Found", "Discount bucket code not found")
+          )
+        ),
+        TE.chainW(code =>
+          pipe(
+            TE.tryCatch(
+              () =>
+                cgnOperatorDb.query(UpdateDiscountBucketCodeSetUsed, {
                   raw: true,
                   replacements: { bucket_code_k: code.bucket_code_k },
                   transaction: t,
                   type: QueryTypes.UPDATE
-                }
-              );
-
-              if (numberOfUpdatedRecords !== 1) {
-                // we expect just one code to be updated, or else we should rollback
-                throw Error("Cannot update the bucket code");
-              }
-
-              return O.some(code);
-            })
-          )()
+                }),
+              E.toError
+            ),
+            TE.chain(([__, numberOfUpdatedRecords]) =>
+              TE.fromPredicate(
+                (updatedRecordNumber: number) => updatedRecordNumber === 1,
+                () => new Error("Cannot update the bucket code")
+              )(numberOfUpdatedRecords)
+            ),
+            TE.mapLeft(err => ResponseErrorInternal(err.message)),
+            TE.chainW(() =>
+              pipe(
+                code,
+                DiscountBucketCode.decode,
+                TE.fromEither,
+                TE.bimap(
+                  e => ResponseErrorInternal(errorsToError(e).message),
+                  ResponseSuccessJson
+                )
+              )
+            )
+          )
         ),
-      E.toError
-    ),
-    TE.mapLeft(e => ResponseErrorInternal(e.message)),
-    TE.chainW(
-      TE.fromOption(() =>
-        ResponseErrorNotFound("Not Found", "Discount bucket code not found")
-      )
-    ),
-    TE.chainW(
-      flow(
-        DiscountBucketCode.decode,
-        TE.fromEither,
-        TE.bimap(
-          e => ResponseErrorInternal(errorsToError(e).message),
-          ResponseSuccessJson
+        TE.chainW(success =>
+          pipe(
+            commitTransaction(t),
+            TE.bimap(
+              err => ResponseErrorInternal(err.message),
+              () => success
+            )
+          )
+        ),
+        TE.orElseW(errorResponses =>
+          pipe(
+            rollbackTransaction(t),
+            TE.mapLeft(err => ResponseErrorInternal(err.message)),
+            TE.chain(() => TE.left(errorResponses))
+          )
         )
       )
     ),
