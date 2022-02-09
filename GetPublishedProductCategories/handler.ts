3af710679bea5ce5e03f0599f53a18b3fa2e0f8e
Michael Disaro <michael.disaro@gmail.com>
2022-02-09 17:22:51 +0100

    fixed linting

--- GetPublishedProductCategories/handler.ts
+++ GetPublishedProductCategories/handler.ts
@@ -3,9 +3,7 @@ import * as express from "express";
 import * as E from "fp-ts/lib/Either";
 import * as TE from "fp-ts/lib/TaskEither";
 import * as AR from "fp-ts/lib/Array";
-import {
-  wrapRequestHandler
-} from "@pagopa/io-functions-commons/dist/src/utils/request_middleware";
+import { wrapRequestHandler } from "@pagopa/io-functions-commons/dist/src/utils/request_middleware";
 import { Sequelize, QueryTypes } from "sequelize";
 
 import { pipe, flow } from "fp-ts/lib/function";
@@ -20,7 +18,6 @@ import PublishedProductCategoryModel from "../models/PublishedProductCategoryMod
 import { SelectPublishedProductCategories } from "../utils/postgres_queries";
 import { errorsToError } from "../utils/conversions";
 import { PublishedProductCategories } from "../generated/definitions/PublishedProductCategories";
-
 
 type ResponseTypes =
   | IResponseSuccessJson<PublishedProductCategories>
@@ -43,12 +40,18 @@ export const GetPublishedProductCategoriesHandler = (
     ),
     TE.map(
       flow(
-        AR.map(productCategoryModel => ProductCategoryFromModel(productCategoryModel.product_category)),
+        AR.map(productCategoryModel =>
+          ProductCategoryFromModel(productCategoryModel.product_category)
+        ),
         productCategories => ({ items: productCategories })
       )
     ),
     TE.chain(
-      flow(PublishedProductCategories.decode, TE.fromEither, TE.mapLeft(errorsToError))
+      flow(
+        PublishedProductCategories.decode,
+        TE.fromEither,
+        TE.mapLeft(errorsToError)
+      )
     ),
     TE.bimap(e => ResponseErrorInternal(e.message), ResponseSuccessJson),
     TE.toUnion
