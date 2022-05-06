b5dcf3a6fe95c5f01365f681098f2bd4fa05a272
Michael Disaro <michael.disaro@gmail.com>
2022-05-06 16:33:24 +0200

    fixed type

--- models/PublishedProductCategoryModel.ts
+++ models/PublishedProductCategoryModel.ts
@@ -1,8 +1,7 @@
-import { NonNegativeInteger } from "@pagopa/ts-commons/lib/numbers";
 import { Model } from "sequelize";
 import { ProductCategoryEnumModelType } from "./ProductCategories";
 
 export default class PublishedProductCategoryModel extends Model {
   public readonly product_category!: ProductCategoryEnumModelType;
-  public readonly new_discounts!: NonNegativeInteger;
+  public readonly new_discounts!: number;
 }
